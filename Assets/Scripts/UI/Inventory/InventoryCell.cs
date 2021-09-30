8d8d06b0c87500c8bc75b6ebfd992d2ac9e1a946
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-09-30 12:32:39 +0300

    Add InventoryCell prefab with script

---
+++ Assets/Scripts/UI/Inventory/InventoryCell.cs
@@ -0,0 +1,19 @@
+﻿using Db.Inventory;
+using TMPro;
+using UnityEngine;
+
+namespace UI.Inventory {
+	public class InventoryCell : MonoBehaviour {
+		[Header("Dependent objects")]
+		[SerializeField] private TextMeshProUGUI itemName;
+		[SerializeField] private TextMeshProUGUI itemAmount;
+		
+		private void UpdateInventoryCell(InventoryItemVo inventoryItemVo) {
+			var name = inventoryItemVo.Name;
+			var amount = inventoryItemVo.Amount;
+
+			itemName.text = name;
+			itemAmount.text = amount.ToString();
+		}
+	}
+}
