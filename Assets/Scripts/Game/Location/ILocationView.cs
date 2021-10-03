7e45187b7557e2751ea572ff12d4cbf20af23430
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-10-03 23:30:05 +0300

    Revert "Remove unused Location"
    This reverts commit 0b8c3bf66c7c6b38aff40909c53ecf6427c6c0ce.

---
+++ Assets/Scripts/Game/Location/ILocationView.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using Game.Location.Place;
+
+namespace Game.Location {
+	public interface ILocationView {
+		List<ObjectSpawnPoint> ObjectSpawnPoints { get; }
+	}
+}
