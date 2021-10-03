e04aa5d8f0d320cb8f3deb44f925eb1e769f812c
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-10-03 21:47:31 +0300

    Add Damage to FragGrenade

--- Assets/Scripts/Game/Weapon/FragGrenade.cs
+++ Assets/Scripts/Game/Weapon/FragGrenade.cs
@@ -3,18 +3,31 @@ using UnityEngine;
 
 namespace Game.Weapon {
 	public class FragGrenade : AGrenade {
-		private static readonly Collider[] SphereCastPool = new Collider[32];
-
+		private static Collider[] _sphereCastPool = new Collider[DefaultSphereCastPoolSize];
+		private const int DefaultSphereCastPoolSize = 10;
+		
 		[SerializeField] private float radius;
 		[SerializeField] private float damage;
-		
+
 		protected override void OnExplosion(Collision _) {
-			int count = Physics.OverlapSphereNonAlloc(transform.position, radius, SphereCastPool);
+			int count = Physics.OverlapSphereNonAlloc(transform.position, radius, _sphereCastPool);
+			
+			while (count == _sphereCastPool.Length) {
+				DoubledSphereCastPoolSize();
+				count = Physics.OverlapSphereNonAlloc(transform.position, radius, _sphereCastPool);
+			}
+			
 			for (int i = 0; i < count; ++i)
 			{
-				Damageable t = SphereCastPool[i].GetComponent<Damageable>();
-				t.Damage(damage);
+				if (_sphereCastPool[i].TryGetComponent<Damageable>(out var damageableObject)) {
+					damageableObject.Damage(damage);
+				}
 			}
+		}
+
+		private static void DoubledSphereCastPoolSize() {
+			var lastSize = _sphereCastPool.Length;
+			_sphereCastPool = new Collider[lastSize * lastSize];
 		}
 	}
 }
