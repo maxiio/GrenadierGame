6bac1191e1d2c7d2034655e9c34105a98df84165
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-10-03 21:07:12 +0300

    Add Grenade classes

---
+++ Assets/Scripts/Game/Weapon/AGrenade.cs
@@ -0,0 +1,26 @@
+﻿using Game.Throwing;
+using UnityEngine;
+
+namespace Game.Weapon {
+	public abstract class AGrenade : Throwable {
+		[SerializeField] private GameObject explosionFxPrefab;
+		
+		private void OnCollisionEnter(Collision collision) {
+			Explosion(collision);
+		}
+
+		protected abstract void OnExplosion(Collision collision);
+
+		private void Explosion(Collision collision) {
+			CreateFx();
+			OnExplosion(collision);
+			Destroy(gameObject);
+		}
+
+		private void CreateFx() {
+			var explosionFx = Instantiate(explosionFxPrefab);
+			explosionFx.transform.position = transform.position;
+			explosionFx.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Weapon/FragGrenade.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.Weapon {
+	public class FragGrenade : AGrenade {
+		protected override void OnExplosion(Collision collision) {
+			
+		}
+	}
+}
