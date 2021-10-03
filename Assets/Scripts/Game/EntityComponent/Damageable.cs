7d42f747726ae1550e3d0d936e4d1e1687f3b05b
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-10-03 21:30:46 +0300

    Add Damageable script, realize damage from FragGrenade, add simple explosion fx

---
+++ Assets/Scripts/Game/EntityComponent/Damageable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.EntityComponent {
+	public class Damageable : MonoBehaviour {
+		public UnityEvent<float> healthChanged;
+		
+		[SerializeField] private float healthPoints;
+
+		public void Damage(float damage) {
+			healthPoints -= damage;
+			healthChanged.Invoke(-damage);
+			if (healthPoints > 0) {
+				return;
+			}
+
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/Game/Weapon/FragGrenade.cs
+++ Assets/Scripts/Game/Weapon/FragGrenade.cs
@@ -1,9 +1,20 @@
+using Game.EntityComponent;
 using UnityEngine;
 
 namespace Game.Weapon {
 	public class FragGrenade : AGrenade {
-		protected override void OnExplosion(Collision collision) {
-			
+		private static readonly Collider[] SphereCastPool = new Collider[32];
+
+		[SerializeField] private float radius;
+		[SerializeField] private float damage;
+		
+		protected override void OnExplosion(Collision _) {
+			int count = Physics.OverlapSphereNonAlloc(transform.position, radius, SphereCastPool);
+			for (int i = 0; i < count; ++i)
+			{
+				Damageable t = SphereCastPool[i].GetComponent<Damageable>();
+				t.Damage(damage);
+			}
 		}
 	}
 }
